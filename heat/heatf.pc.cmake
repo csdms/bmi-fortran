9f949cb49ca0ecc40b5a37ba37cae590b6773d62
Mark Piper <mark.piper@colorado.edu>
2017-03-21 12:13:56 -0600

    Fix library name

--- heat/heatf.pc.cmake
+++ heat/heatf.pc.cmake
@@ -1,5 +1,5 @@
 Name: heat
 Description: An example of the heat equation, with a BMI, in Fortran
 Version: ${HEAT_VERSION}
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -l${bmiheatlib}
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -l${bmiheat_lib}
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include
