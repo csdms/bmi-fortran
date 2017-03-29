8629da40f7314c77ca80838e8c31fa6d1c35603a
Mark Piper <mark.piper@colorado.edu>
2017-03-29 13:34:42 -0600

    Add test to show that instances don't conflict

---
+++ testing/conflicting_instances_test.f90
@@ -0,0 +1,68 @@
+! Do two instances of bmi_heat conflict?
+program conflicting_instances_test
+
+  use bmiheatf
+  use testing_helpers
+  implicit none
+
+  type (bmi_heat) :: m1
+  type (bmi_heat) :: m2
+  character (len=BMI_MAXVARNAMESTR) :: &
+       cfg_file1 = "test1.cfg", cfg_file2 = "test2.cfg"
+  integer :: s
+  integer :: grid_id1, grid_id2
+  character (len=BMI_MAXVARNAMESTR), pointer :: names1(:), names2(:)
+  integer :: dims1(2), dims2(2)
+  real, pointer :: z1(:), z2(:)
+  character(len=30) :: rowfmt1, rowfmt2
+
+  write(*, "(a, a10, a10)") "Configuration files: ", cfg_file1, cfg_file2
+
+  write (*,"(a)",advance="no") "Initializing..."
+  s = m1%initialize(cfg_file1)
+  s = m2%initialize(cfg_file2)
+  write (*,*) "Done."
+
+  s = m1%get_output_var_names(names1)
+  s = m1%get_var_grid(names1(1), grid_id1)
+  s = m1%get_grid_shape(grid_id1, dims1)
+  write(rowfmt1,'(a,i4,a)') '(', dims1(2), '(1x,f6.1))'
+
+  write (*, "(a)") "Initial values, model 1:"
+  s = m1%get_value("plate_surface__temperature", z1)
+  call print_array(z1, dims1)
+
+  s = m2%get_output_var_names(names2)
+  s = m2%get_var_grid(names2(1), grid_id2)
+  s = m2%get_grid_shape(grid_id2, dims2)
+  write(rowfmt2,'(a,i4,a)') '(', dims2(2), '(1x,f6.1))'
+
+  write (*, "(a)") "Initial values, model 2:"
+  s = m2%get_value("plate_surface__temperature", z2)
+  call print_array(z2, dims2)
+
+  write (*, "(a)") "Set new value in model 2; does it affect model 1?"
+  s = m2%set_value_at_indices("plate_surface__temperature", [20], [42.0])
+  write (*, "(a)") "New values, model 2:"
+  s = m2%get_value("plate_surface__temperature", z2)
+  call print_array(z2, dims2)
+  write (*, "(a)") "New values, model 1:"
+  s = m1%get_value("plate_surface__temperature", z1)
+  call print_array(z1, dims1)
+
+  write (*, "(a)") "Update both models by one time step..."
+  s = m1%update()
+  s = m2%update()
+  write (*, "(a)") "Updated values, model 1:"
+  s = m1%get_value("plate_surface__temperature", z1)
+  call print_array(z1, dims1)
+  write (*, "(a)") "Updated values, model 2:"
+  s = m2%get_value("plate_surface__temperature", z2)
+  call print_array(z2, dims2)
+
+  write (*,"(a)", advance="no") "Finalizing..."
+  s = m1%finalize()
+  s = m2%finalize()
+  write (*,*) "Done"
+
+end program conflicting_instances_test
