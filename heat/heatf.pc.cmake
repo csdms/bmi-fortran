625840f038bb1994bdc7f0cf649216804282e1e8
Mark Piper <mark.piper@colorado.edu>
2018-09-17 22:22:03 -0600

    Don't forget to link libbmiheatf against libbmif

--- heat/heatf.pc.cmake
+++ heat/heatf.pc.cmake
@@ -1,5 +1,5 @@
 Name: heat
 Description: An example of the heat equation, with a BMI, in Fortran
 Version: ${HEAT_VERSION}
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -l${bmiheat_lib}
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -l${bmiheat_lib} -l${bmi_lib}
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include
