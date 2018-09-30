b70c387165a4df86c8a43a1a97dfe8cb49a2211d
Mark Piper <mark.piper@colorado.edu>
2018-09-30 16:33:08 -0600

    Add tests for start, end, and current time methods
    Now that I have a framework, it's easy to add tests.

---
+++ heat/tests/test_get_end_time.f90
@@ -0,0 +1,20 @@
+program test_get_end_time
+
+  use bmiheatf
+  use fixtures
+
+  implicit none
+
+  integer, parameter :: expected_time = 20.0
+
+  type (bmi_heat) :: m
+  real :: end_time
+
+  status = m%initialize(config_file)
+  status = m%get_end_time(end_time)
+  status = m%finalize()
+
+  if (end_time.ne.expected_time) then
+     stop 1
+  end if
+end program test_get_end_time
