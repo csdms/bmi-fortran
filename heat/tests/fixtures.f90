97c30f8393ce3df3296f19362204cb427b0913fb
Mark Piper <mark.piper@colorado.edu>
2018-10-02 21:44:59 -0600

    Make test/example to show that get_value and get_value_ref work correctly

--- heat/tests/fixtures.f90
+++ heat/tests/fixtures.f90
@@ -6,4 +6,19 @@ module fixtures
 
   integer :: status
 
+contains
+
+  subroutine print_array(array, dims)
+    integer :: dims(2)
+    real, dimension(product(dims)) :: array
+    integer :: i, j
+
+    do j = 1, dims(1)
+       do i = 1, dims(2)
+          write (*,"(f6.1)", advance="no") array(j + dims(1)*(i-1))
+       end do
+       write (*,*)
+    end do
+  end subroutine print_array
+
 end module fixtures
---
+++ heat/tests/test_by_reference.f90
@@ -0,0 +1,41 @@
+program test_by_reference
+
+  use bmiheatf
+  use fixtures, only: status, print_array
+
+  implicit none
+
+  character (len=*), parameter :: config_file = "sample.cfg"
+  character (len=*), parameter :: var_name = "plate_surface__temperature"
+  integer, parameter :: grid_id = 0
+  integer, parameter :: rank = 2
+
+  type (bmi_heat) :: m
+  integer, dimension(rank) :: shape
+  real, pointer :: tval(:), tref(:)
+
+  status = m%initialize(config_file)
+  status = m%get_grid_shape(grid_id, shape)
+
+  ! Get initial temperature array, by value and by reference.
+  status = m%get_value("plate_surface__temperature", tval)
+  status = m%get_value_ref("plate_surface__temperature", tref)
+  write (*, "(a)") "Time = 0 (by value):"
+  call print_array(tval, shape)
+  write (*, "(a)") "Time = 0 (by reference):"
+  call print_array(tref, shape)
+
+  ! Advance model by a time step and compare tval and tref (they
+  ! should differ because tval won't have been updated).
+  status = m%update()
+  write (*, "(a)") "Time = 1 (by value):"
+  call print_array(tval, shape)
+  write (*, "(a)") "Time = 1 (by reference):"
+  call print_array(tref, shape)
+  
+  status = m%finalize()
+
+  if (sum(tval).eq.sum(tref)) then
+     stop 1
+  end if
+end program test_by_reference
