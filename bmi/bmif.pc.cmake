1c2f14c1f04ca5df750349e159c043e907fa4c7f
Mark Piper <mark.piper@colorado.edu>
2018-10-05 18:52:37 -0600

    Compile against the Fortran 2003 standard
    Although it would be nice, I don't want to use Fortran 2008 or later
    in this project.

--- bmi/bmif.pc.cmake
+++ bmi/bmif.pc.cmake
@@ -2,4 +2,4 @@ Name: BMI
 Description: Basic Model Interface for Fortran
 Version: ${BMI_VERSION}
 Libs: -L${CMAKE_INSTALL_PREFIX}/lib -l${bmi_lib}
-Cflags: -I${CMAKE_INSTALL_PREFIX}/include
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include -std=f2003
--- heat/heatf.pc.cmake
+++ heat/heatf.pc.cmake
@@ -2,4 +2,4 @@ Name: heat
 Description: An example of the heat equation, with a BMI, in Fortran
 Version: ${HEAT_VERSION}
 Libs: -L${CMAKE_INSTALL_PREFIX}/lib -l${bmiheat_lib} -l${bmi_lib}
-Cflags: -I${CMAKE_INSTALL_PREFIX}/include
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include -std=f2003
