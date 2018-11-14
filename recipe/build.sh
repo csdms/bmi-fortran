4ec6c7dab04c547e113a9e2a712a0f36d914d816
Mark Piper <mark.piper@colorado.edu>
2018-11-14 12:33:58 -0700

    Uncompress installed Fortran .mod files
    Why are they being compressed? Several google searches didn't yield a
    clue. Note that this only happens on Linux, not on macOS. I've
    manually uncompressed the .mod files in the build script.
    
    Incremented the build number.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,3 +4,13 @@ mkdir _build && cd _build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
 make
 make install
+
+# Why are Fortran .mod files being compressed?
+if [[ `uname -s` == 'Linux' ]]; then
+    cd $PREFIX/lib
+    mod_files=`ls -1 *.mod`
+    for f in $mod_files; do
+	mv $f $f.gz
+	gunzip $f.gz
+    done
+fi
