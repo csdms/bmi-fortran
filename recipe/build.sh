752385c1c84ad3a75b31c26d4e9cdb994d3be4b2
Mark Piper <mark.piper@colorado.edu>
2019-05-10 09:42:03 -0600

    Process module files prefixed with bmif
    With the new naming scheme for the BMI module file, there isn't
    a straightforward way to get the name of the file. This is sort
    of a band-aid, but it should be reliable.

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,9 @@ make install
 # Why are Fortran .mod files being compressed by conda-build?
 if [[ `uname -s` == 'Linux' ]]; then
     cd $PREFIX/include
-    f=bmif.mod
-    mv $f $f.gz
-    gunzip $f.gz
+    mod_files=`ls -1 bmif*.mod`
+    for f in $mod_files; do
+	mv $f $f.gz
+	gunzip $f.gz
+    done
 fi
