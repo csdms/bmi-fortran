383c7119726843172d1f06bc9a43feeb8c6e42d5
Mark Piper <mark.piper@colorado.edu>
2024-12-10 21:38:17 -0700

    Use new BMI docs location

--- bmif.pc.cmake
+++ bmif.pc.cmake
@@ -5,7 +5,7 @@ includedir=${prefix}/include
 
 Name: bmi-fortran
 Description: The Basic Model Interface for Fortran
-URL: https://bmi.readthedocs.io
+URL: https://bmi.csdms.io
 Version: @CMAKE_PROJECT_VERSION_MAJOR@.@CMAKE_PROJECT_VERSION_MINOR@
 Libs: -L${libdir} -l@CMAKE_PROJECT_NAME@
 Cflags: -I${includedir}
